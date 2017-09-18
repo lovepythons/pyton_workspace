afbce4489ed19439a57e74e12a4970db2a66b87c
bandishui_company <bandishui@139.com>
2017-09-18 10:58:12 +0800

    update code1.py v0.2

--- code/code1.py
+++ code/code1.py
@@ -1,25 +1,42 @@
 # -*- coding: utf-8 -*-
 # @Author: bandishui_home
 # @Date:   2017-09-17 22:04:41
-# @Last Modified by:   bandishui_home
-# @Last Modified time: 2017-09-17 22:19:11
+# @Last Modified by:   Administrator
+# @Last Modified time: 2017-09-18 10:57:04
 
 
 
 import random
 import sys
 
-def fun1():
+def strEncode(str):
+    '''
+    文字转码到控制台输出的格式
+    '''
     type = sys.getfilesystemencoding()
-    n = int( raw_input('从多少数字中挑选？ input:'.decode('utf-8').encode(type)) );
-    m = int( raw_input('挑选出多少个数字？ input:'.decode('utf-8').encode(type)) );
+    return str.decode('utf-8').encode(type);
 
-    n = 1+n;
+def fun1():
+    '''
+    从N个数字中随机选出M个数字
+    '''
+    while True:
+        try:
+            n = int( raw_input( strEncode('从多少数字中挑选？(大于等于0) n=') ) );
+            m = int( raw_input( strEncode('挑选出多少个数字？(大于等于0,小于等于n) m=') ) );
+        except:
+            print strEncode('请输入数字!');
+            print '';
+        if( n>=0 and m<=n ):
+            break;
+        else:
+            print strEncode('n大于等于0, 并且 m小于等于n! ');
+
     res = set();
     while( len(res)<m ):
-        res.add(random.choice(xrange(1,n)));
+        res.add(random.randint(1,n));
 
-    print list(res);
+    print sorted(list(res));
 
 
 
